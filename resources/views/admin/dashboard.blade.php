c0dc0c0d172cae4de8397d1025aada0dedfa49b5
trigunadharma <hafiz.gunawan03@gmail.com>
2024-05-28 22:53:27 +0700

    update chart dan sweet alert

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,25 +1,158 @@
 @extends('admin.layouts.app')
-
 @section('konten')
 
-    <h1 class="mt-4">Static Navigation</h1>
-    <ol class="breadcrumb mb-4">
-        <li class="breadcrumb-item"><a href="/dashboard">Dashboard</a></li>
-        <li class="breadcrumb-item active">Static Navigation</li>
-    </ol>
-    <div class="card mb-4">
-        <div class="card-body">
-            <p class="mb-0">
-                This page is an example of using static navigation. By removing the
-                <code>.sb-nav-fixed</code>
-                class from the
-                <code>body</code>
-                , the top navigation and side navigation will become static on scroll. Scroll down this page to see an example.
-            </p>
-        </div>
-    </div>
-    <div style="height: 100vh"></div>
-    <div class="card mb-4"><div class="card-body">When scrolling, the navigation stays at the top of the page. This is the end of the static navigation demo.</div></div>
+<div class="container-fluid px-4">
+                        <h1 class="mt-4">Dashboard</h1>
+                        <ol class="breadcrumb mb-4">
+                            <li class="breadcrumb-item active">Dashboard</li>
+                        </ol>
+                        <div class="row">
+                            <div class="col-xl-3 col-md-6">
+                                <div class="card bg-primary text-white mb-4">
+                                    <div class="card-body">Data Layanan : {{$layanan}}</div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link" href="{{route('layanan.index')}}">
+                                            View Details</a>
+                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-md-6">
+                                <div class="card bg-warning text-white mb-4">
+                                    <div class="card-body">Data Pemesanan : {{$pemesanan}}</div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link" href="{{route('pemesanan.index')}}">
+                                            View Details</a>
+                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-md-6">
+                                <div class="card bg-success text-white mb-4">
+                                    <div class="card-body">Data Transaksi : {{$transaksi}}</div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link" href="{{url('admin/transaksi')}}">
+                                            View Details</a>
+                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-xl-3 col-md-6">
+                                <div class="card bg-danger text-white mb-4">
+                                    <div class="card-body">Data Users : {{$users}}</div>
+                                    <div class="card-footer d-flex align-items-center justify-content-between">
+                                        <a class="small text-white stretched-link" href="{{url('admin/users')}}">
+                                            View Details</a>
+                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-xl-8">
+                                <div class="card mb-4">
+                                    <div class="card-header">
+                                        <i class="fas fa-chart-area me-1"></i>
+                                        steam layanan chart
+                                    </div>
+                                    <div class="card-body"><canvas id="myAreaChart" width="100%" height="40"></canvas></div>
+                                </div>
+                            </div>
+                            <div class="col-xl-4">
+                                <div class="card mb-4">
+                                    <div class="card-header">
+                                        <i class="fas fa-chart-bar me-1"></i>
+                                        Jenis Mobil Chart
+                                    </div>
+                                    <div class="card-body"><canvas id="myPieChart" width="80%" height="20"></canvas></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+<script>
+    // Set new default font family and font color to mimic Bootstrap's default styling
+// Chart.defaults.global.defaultFontFamily = '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+// Chart.defaults.global.defaultFontColor = '#292b2c';
 
+var pemesananData = @json($pemesananData);
+//kodingan diatas memanggil variable pemesananData yang dikirim compact dari 
+//DashboardController
+//json mengubah data dari data array ke json untuk dideklarasikan ke javascript
+var labels = pemesananData.map(function(item){
+    return item.tanggal_awal_booking; 
+});
+//var labels mendeklarasikan data yang mengambil kolom kode 
+var data = pemesananData.map(function(item){
+    return item.layanan_id;
+//map ada fungsi untuk mengurutkan data
+});
+// var data mendeklarasikan data yang mengambil kolom harga_jual
+// Area Chart Example
+var ctx = document.getElementById("myAreaChart");
+var myLineChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: labels,
+    datasets: [{
+      label: "Layanan",
+      lineTension: 0.3,
+      backgroundColor: "rgba(2,117,216,0.2)",
+      borderColor: "rgba(2,117,216,1)",
+      pointRadius: 5,
+      pointBackgroundColor: "rgba(2,117,216,1)",
+      pointBorderColor: "rgba(255,255,255,0.8)",
+      pointHoverRadius: 5,
+      pointHoverBackgroundColor: "rgba(2,117,216,1)",
+      pointHitRadius: 50,
+      pointBorderWidth: 2,
+      data: data,
+    }],
+  },
+  options: {
+    scales: {
+      xAxes: [{
+        time: {
+          unit: 'date'
+        },
+        gridLines: {
+          display: false
+        },
+        ticks: {
+          maxTicksLimit: 7
+        }
+      }],
+      yAxes: [{
+        ticks: {
+          min: 0,
+          max: 40000,
+          maxTicksLimit: 5
+        },
+        gridLines: {
+          color: "rgba(0, 0, 0, .125)",
+        }
+      }],
+    },
+    legend: {
+      display: false
+    }
+  }
+});
 
+</script>
+<script>
+    var label = [@foreach($jenis_mobil as $jm) '{{$jm->jenis_mobil}}', @endforeach];
+    var data = [@foreach($jenis_mobil as $jm) '{{$jm->jumlah}}', @endforeach];
+var ctx = document.getElementById("myPieChart");
+var myPieChart = new Chart(ctx, {
+  type: 'pie',
+  data: {
+    labels: label,
+    datasets: [{
+      data: data,
+      backgroundColor: ['#007bff', '#dc3545', '#ffc107', '#28a745'],
+    }],
+  },
+});
+
+</script>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -6,35 +6,11 @@ use App\Http\Controllers\TransaksiController;
 use App\Http\Controllers\UsersController;
 use App\Http\Controllers\PemesananController;
 
-
-
-Route::get('/', function () {
-    return view('/front.home');
-});
-
-Route::get('/home', function () {
-    return view('/front/home');
-});
-
-Route::get('/about', function () {
-    return view('/front/about');
-});
-
-Route::get('/service', function () {
-    return view('/front/service');
-});
-
-Route::get('/contact', function () {
-    return view('/front/contact');
-});
-
-Route::get('/dashboard', function () {
-    return view('admin.dashboard');
-});
+use App\Http\Controllers\DashboardController;
 
 
 Route::prefix('admin')->group(function(){
-
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
 
 Route::resource('layanan', LayananController::class);
 Route::resource('transaksi', TransaksiController::class);
@@ -48,6 +24,7 @@ Route::post('/users/store', [UsersController::class, 'store']);
 
 
 });
+
 
 
 
