888828e877dc9858cb29543b6af50eee24813980
riss aja <buatyt542@gma.com>
2024-06-08 17:31:30 +0700

    update project

--- resources/views/front/service.blade.php
+++ resources/views/front/service.blade.php
@@ -4,7 +4,7 @@
 <div class="container-xxl service py-5">
     <div class="container">
         <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
-            <h6 class="text-primary text-uppercase">// Our Services //</h6>
+            <h6 class="text-primary text-uppercase">// Layanan Kami //</h6>
             <h1 class="mb-5">Explore Our Services</h1>
         </div>
         <div class="row g-4 wow fadeInUp" data-wow-delay="0.3s">
